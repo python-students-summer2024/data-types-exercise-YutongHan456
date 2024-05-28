3cf0d4fdcdbfc1fbd0a4a9b2f755e9205c2ba186
YutongHan456 <yh4980@nyu.edu>
2024-05-28 18:33:36 -0400

    homework3

--- solutions.py
+++ solutions.py
@@ -11,6 +11,13 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
+    total_sales = input("Please input the total sales")
+    annual_profit = float(total_sales) * 0.23
+    annual_profit_rounded = str("%.2f" % annual_profit)
+    total_sales_format = "$" + str(total_sales)
+    annual_profit_rounded_format = "$" + str(annual_profit_rounded)
+    print("Profit:",annual_profit_rounded_format,"for sales of",total_sales_format)
+
 
 
 def calculate_quotient_and_remainder():
@@ -22,6 +29,12 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
+    number_one = int(input("Please enter number 1"))
+    number_two = int(input("Please enter number 2"))
+    quotient = int(number_one/number_two)
+    remainder = number_one % number_two
+    print(number_two,"goes into",number_one,"a total of",quotient,"times with a remainder of",remainder)
+
 
 
 def calculate_miles_per_gallon():
@@ -35,6 +48,12 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
+    miles_driven = int(input("Please enter the miles driven"))
+    gas_used = int(input("please enter the gas used"))
+    mpg = miles_driven/gas_used
+    print("Miles driven:",miles_driven)
+    print("Gas used(gallons):",gas_used)
+    print("Miles per gallon:",mpg)
 
 
 def align_text():
@@ -53,3 +72,12 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+    price_one = float(input("Please enter number 1"))
+    price_two = float(input("Please enter number 2"))
+    price_three = float(input("Please enter number 3"))
+    price_one_rounded = "%.2f" % price_one
+    price_two_rounded = "%.2f" % price_two
+    price_three_rounded = "%.2f" % price_three
+    print("Price #1: $",price_one_rounded.rjust(10))
+    print("Price #2: $",price_two_rounded.rjust(10))
+    print("Price #3: $",price_three_rounded.rjust(10))
